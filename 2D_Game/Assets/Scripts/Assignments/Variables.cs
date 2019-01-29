f522cb0b6005fd718bb1ced6e35ad1ef34bef90e
Preston Powell <Preston.powell@uvu.edu>
2019-01-28 17:29:52 -0700

    Variables

---
+++ 2D_Game/Assets/Scripts/Assignments/Variables.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour{
+	// Variables
+	public int number; 
+	public float speed; 
+	public string name;
+
+	// Use this for initialization
+	void Start () {
+		/*
+		This
+		is 
+		a
+		multiline
+		comment		
+		 */
+		// number = 10;
+		speed = 0.94f;
+		name = "Bob";
+
+		print(name +" is "+number+" years old!"+ speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(number,0,0);
+		
+	}
+}
