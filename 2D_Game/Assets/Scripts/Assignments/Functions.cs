62640ff00615de0c2b8599d5b1ef183b7672153f
Preston Powell <Preston.powell@uvu.edu>
2019-01-30 17:11:19 -0700

    Added comment to function.cs

--- 2D_Game/Assets/Scripts/Assignments/Functions.cs
+++ 2D_Game/Assets/Scripts/Assignments/Functions.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,7 +6,7 @@ public class Functions : MonoBehaviour {
 
 	public string weather;
 
-
+// Call weather function
 	void Start(){
 
 		Weather(weather);
