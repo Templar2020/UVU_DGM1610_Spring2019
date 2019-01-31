2191de35e65db9a597c3c4dd26cf34f08d1806bc
Preston Powell <Preston.powell@uvu.edu>
2019-01-30 17:08:29 -0700

    Added function and conditional examples

---
+++ 2D_Game/Assets/Scripts/Assignments/Functions.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Functions : MonoBehaviour {
+
+	public string weather;
+
+
+	void Start(){
+
+		Weather(weather);
+	}
+
+	void Weather(string weatherState){
+
+		if(weatherState == "Sunny"){
+			print("The sun is shining today!");
+		}
+		else if(weatherState == "Raining"){
+			print("It is soggy and wet today!");
+		}
+		else if(weatherState == "Windy"){
+			print("It is very windy today!");
+		}
+		else if(weatherState == "Snowing"){
+			print("It is a blizzard out there!");
+		}
+		else if(weatherState == "Foggy"){
+			print("Visability is very poor due to fog");
+		}
+		else{
+			print("I don't understand "+ weatherState);
+		}
+
+	}
+}
