2ac99663b098aa10b39139672abf74c63febcc23
Preston Powell <Preston.powell@uvu.edu>
2019-02-06 17:34:47 -0700

    Added Basic Player Controller

--- 2D_Game/Assets/Scripts/Assignments/Functions.cs
+++ 2D_Game/Assets/Scripts/Assignments/Functions.cs
@@ -4,34 +4,34 @@ using UnityEngine;
 
 public class Functions : MonoBehaviour {
 
-	public string weather;
+// 	public string weather;
 
-// Call weather function
-	void Start(){
+// // Call weather function
+// 	void Start(){
 
-		Weather(weather);
-	}
+// 		Weather(weather);
+// 	}
 
-	void Weather(string weatherState){
+// 	void Weather(string weatherState){
 
-		if(weatherState == "Sunny"){
-			print("The sun is shining today!");
-		}
-		else if(weatherState == "Raining"){
-			print("It is soggy and wet today!");
-		}
-		else if(weatherState == "Windy"){
-			print("It is very windy today!");
-		}
-		else if(weatherState == "Snowing"){
-			print("It is a blizzard out there!");
-		}
-		else if(weatherState == "Foggy"){
-			print("Visability is very poor due to fog");
-		}
-		else{
-			print("I don't understand "+ weatherState);
-		}
+// 		if(weatherState == "Sunny"){
+// 			print("The sun is shining today!");
+// 		}
+// 		else if(weatherState == "Raining"){
+// 			print("It is soggy and wet today!");
+// 		}
+// 		else if(weatherState == "Windy"){
+// 			print("It is very windy today!");
+// 		}
+// 		else if(weatherState == "Snowing"){
+// 			print("It is a blizzard out there!");
+// 		}
+// 		else if(weatherState == "Foggy"){
+// 			print("Visability is very poor due to fog");
+// 		}
+// 		else{
+// 			print("I don't understand "+ weatherState);
+// 		}
 
-	}
+// 	}
 }
---
+++ 2D_Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed; 
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.D)){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+		else if(Input.GetKey(KeyCode.A)){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+	}
+}
