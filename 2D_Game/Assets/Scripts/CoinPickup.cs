afe13d78f13f4ec698bd29c962beda05dc584858
Preston Powell <Preston.powell@uvu.edu>
2019-02-20 17:26:27 -0700

    Deleted CoinPickup in Assignments folder.  Created CoinPickup script for game.  Refactored Player Controller.

---
+++ 2D_Game/Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour {
+
+	public int coinValue;
+
+	void OnTriggerEnter2D (Collider2D other){
+		if(other.name == "Player"){
+
+			print("You've Collected the coin!");
+
+			Destroy(gameObject);
+
+		}
+
+	}
+	
+}
--- 2D_Game/Assets/Scripts/PlayerController.cs
+++ 2D_Game/Assets/Scripts/PlayerController.cs
@@ -4,21 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	// Player movement variables
 	public float moveSpeed; 
+	public float jumpHeight;
+
+	//Player grounded variables
+	private bool grounded;
 
 
 	// Use this for initialization
 	void Start () {
-		
+		//Default to true on grounded
+		grounded = true;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+		// Moves player left and right
 		if(Input.GetKey(KeyCode.D)){
 			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 		}
 		else if(Input.GetKey(KeyCode.A)){
 			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 		}
+		
+		// Make player jump 
+		if(Input.GetKeyDown(KeyCode.W) && grounded){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x,jumpHeight);
+		}
+
 	}
 }
