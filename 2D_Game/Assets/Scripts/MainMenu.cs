5fe069334ca52969a0fb741518b92198f903b530
Preston Powell <Preston.powell@uvu.edu>
2019-03-13 17:17:07 -0600

    Added UI Menu, title and buttons with functionality

---
+++ 2D_Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+	//Indexed level to load
+	public int levelToLoad;
+	// This function loads the indexed level stored in the levelToLoad variable after button is pressed
+	public void LoadLevel(){
+		SceneManager.LoadScene(levelToLoad);
+	}
+	// This function quits the game after the button is presssed
+	public void LevelExit(){
+		Application.Quit();
+	}
+}
