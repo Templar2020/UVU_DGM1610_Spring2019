d38d7c17e81d97bdda8e8880b55a4dac5835d06b
Preston Powell <Preston.powell@uvu.edu>
2019-03-25 17:16:59 -0600

    Loop Examples. Added EnemyPatrol script and setup.

---
+++ 2D_Game/Assets/Scripts/Assignments/Loops.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour {
+	
+	public int bottles = 1;
+	public bool isRaining = true; 
+
+	// Use this for initialization
+	void Start () {
+			
+			//For loop
+			// for(int i=100; i > bottles; i--){
+			// 	print(i +" bottles of ginger-beer on the wall");
+			// }
+
+			// while(bottles <= 100){
+			// 	print(bottles +" bottles of ginger-beer on the wall");
+			// 	bottles ++;
+			// }	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ 2D_Game/Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyPatrol : MonoBehaviour {
+
+	// Movement Variables
+	public float moveSpeed;
+	public bool moveRight;
+
+	// Wall Check
+	public Transform wallCheck;
+	public float wallCheckRadius;
+	public LayerMask whatIsWall;
+	private bool hittingWall;
+	
+	// Edge Check
+	private bool notAtEdge;
+	public Transform edgeCheck;
+	
+	
+	// Update is called once per frame
+	void Update () {
+		notAtEdge = Physics2D.OverlapCircle(edgeCheck.position, wallCheckRadius, whatIsWall);
+
+		hittingWall = Physics2D.OverlapCircle(wallCheck.position, wallCheckRadius, whatIsWall);
+
+		if (hittingWall || !notAtEdge){
+			moveRight = !moveRight;
+		}
+
+		if (moveRight){
+			transform.localScale = new Vector3(-10f,10f,1f);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+		else {
+			transform.localScale = new Vector3(10f,10f,1f);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+	}
+}
