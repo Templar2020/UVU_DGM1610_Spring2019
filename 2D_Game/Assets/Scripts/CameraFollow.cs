052bbe7b97c8d9f3d8fb297e2409dace2cba8ce7
Preston Powell <Preston.powell@uvu.edu>
2019-04-03 17:15:01 -0600

    Game Update 4/3

---
+++ 2D_Game/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public PlayerController player;
+
+	public bool isFollowing;
+
+
+	// Camera position offset
+	public float xOffset;
+	public float yOffset;
+
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<PlayerController>();
+
+		isFollowing = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isFollowing){
+			transform.position = new Vector3(player.transform.position.x + xOffset, player.transform.position.y + yOffset, transform.position.z);
+	
+		}
+	}
+
+}
