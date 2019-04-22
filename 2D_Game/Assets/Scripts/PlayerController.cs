d8f8105b67104f2d5e862a2f539981bc2ae2f4a6
Preston Powell <Preston.powell@uvu.edu>
2019-04-22 16:40:36 -0600

    Added Animation Controller and Animations

--- 2D_Game/Assets/Scripts/PlayerController.cs
+++ 2D_Game/Assets/Scripts/PlayerController.cs
@@ -1,57 +1,95 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	// Player movement variables
-	public float moveSpeed; 
+	//Player movement variables
+	public float moveSpeed;
 	public float jumpHeight;
+	private bool doubleJump;
 
 	//Player grounded variables
-	private bool grounded;
 	public Transform groundCheck;
 	public float groundCheckRadius;
 	public LayerMask whatIsGround;
+	private bool grounded;
 
+	//Non-Slide Player
+	private float moveVelocity;
 
+	public Animator animator;
+	
 	// Use this for initialization
 	void Start () {
-		//Default to true on grounded
-		
+		// Animation reset
+		animator.SetBool("isWalking",false);
+		animator.SetBool("isJumping",false);
+
 	}
 
-	void FixedUpdate(){
+	void FixedUpdate () {
 		grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		// Moves player left and right
-		if(Input.GetKey(KeyCode.D)){
-			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-		}
-		else if(Input.GetKey(KeyCode.A)){
-			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-		}
-		
-		// Make player jump 
-		if(Input.GetKeyDown(KeyCode.W) && grounded){
+
+		// This code makes the character jump
+		if(Input.GetKeyDown (KeyCode.Space)&& grounded){
 			Jump();
 		}
+		
+		// Double jump code
+		if(grounded){
+			doubleJump = false;
+			animator.SetBool("isJumping",false);
+		}
+			
+
+		if(Input.GetKeyDown (KeyCode.Space)&& !doubleJump && !grounded){
+			Jump();
+			doubleJump = true;
+		}
+
+		//Non-Slide Player
+		moveVelocity = 0f;	
+
+
+		// This code makes the character move from side to side using the A&D keys
+		if(Input.GetKey (KeyCode.D)){
+			//GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			moveVelocity = moveSpeed;
+			animator.SetBool("isWalking",true);
+		}
+
+		else if(Input.GetKeyUp (KeyCode.D)){
+			animator.SetBool("isWalking",false);
+		}
+
+		if(Input.GetKey (KeyCode.A)){
+			//GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			moveVelocity = -moveSpeed;
+			animator.SetBool("isWalking",true);
+		}
+
+		else if(Input.GetKeyUp (KeyCode.A)){
+			animator.SetBool("isWalking",false);
+		}
+
+		GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
 
 		//Player flip
 		if (GetComponent<Rigidbody2D>().velocity.x > 0)
-			transform.localScale = new Vector3(5f,5f,1f);
+			transform.localScale = new Vector3(1f,1f,1f);
 
 		else if (GetComponent<Rigidbody2D>().velocity.x < 0)
-			transform.localScale = new Vector3(-5f,5f,1f);
-
-
+			transform.localScale = new Vector3(-1f,1f,1f);
 
 	}
 
-	void Jump(){
-		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x,jumpHeight);
+	public void Jump(){
+		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+		animator.SetBool("isJumping",true);
 	}
 }
+
---
+++ 2D_Game/Assets/Scripts/PlayerController_old.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController_old : MonoBehaviour {
+
+	// Player movement variables
+	public float moveSpeed; 
+	public float jumpHeight;
+
+	//Player grounded variables
+	private bool grounded;
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+
+
+	// Use this for initialization
+	void Start () {
+		//Default to true on grounded
+		
+	}
+
+	void FixedUpdate(){
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// Moves player left and right
+		if(Input.GetKey(KeyCode.D)){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+		else if(Input.GetKey(KeyCode.A)){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+		
+		// Make player jump 
+		if(Input.GetKeyDown(KeyCode.W) && grounded){
+			Jump();
+		}
+
+		//Player flip
+		if (GetComponent<Rigidbody2D>().velocity.x > 0)
+			transform.localScale = new Vector3(1f,1f,1f);
+
+		else if (GetComponent<Rigidbody2D>().velocity.x < 0)
+			transform.localScale = new Vector3(-1f,1f,1f);
+
+
+
+	}
+
+	void Jump(){
+		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x,jumpHeight);
+	}
+}
